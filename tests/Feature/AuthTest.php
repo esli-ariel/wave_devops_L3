4f0a597947f6108afac49d13c625734582611978
Kyann2912 <kyann372@gmail.com>
2025-07-12 20:01:09 +0000

    Auto-commit du : 12-07-2025 à 20:01:09

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -50,7 +50,7 @@ class AuthTest extends TestCase
             'type' => 'client',
         ]);
 
-        $response = $this->post('/login', [
+        $response = $this->post('/connexion', [
             'contact' => '0707070707',
             'password' => 'secret123',
         ]);
